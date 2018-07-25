2b868294033d34c966eff14ef351a76277bee4e2
Mariano Benassi <mariano@kpr.com.ar>
2018-07-25 18:10:59 -0300

    Agrego el gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,27 @@
 var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
 var sass = require('gulp-sass');
 
 gulp.task('saludar', function(){
 	console.log('Hola mundo!!!!');
 });
 
+// Static Server + watching scss/html files
+gulp.task('serve', ['sass'], function() {
+    browserSync.init({
+        server: "./app"
+    });
+
+    gulp.watch("app/scss/**/*.scss", ['sass']);
+    //gulp.watch("app/*.html").on('change', browserSync.reload);
+});
+
 
 gulp.task('sass', function(){
 	return gulp.src('scss/**/*.scss')
 		.pipe(sass())
-		.pipe(gulp.dest('app/css'));
+		.pipe(gulp.dest('app/css'))
+		.pipe(browserSync.stream());
 
 });
 
